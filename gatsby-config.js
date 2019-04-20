ed143d087e5dc1c2d0a84593519c3a83841adb0a
Shohei Kondo <s.kondo1015@gmail.com>
2019-04-20 17:09:14 +0900

    site-logoの位置を修正

--- gatsby-config.js
+++ gatsby-config.js
@@ -208,7 +208,7 @@ module.exports = {
         background_color: '#FFF',
         theme_color: '#F7A046',
         display: 'standalone',
-        icon: 'static/site-logo.png',
+        icon: 'static/media/site-logo.png',
       },
     },
     'gatsby-plugin-offline',
