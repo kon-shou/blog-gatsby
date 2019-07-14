8b7e1637c1cbb5e3253c312348c3a426608c3cf0
Shohei Kondo <s.kondo1015@gmail.com>
2019-07-14 15:10:57 +0900

    シンタックスハイライト変更

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,5 +1,7 @@
 'use strict';
 
 require('./src/assets/scss/init.scss');
+require('prism-themes/themes/prism-a11y-dark.css');
+require('./src/assets/scss/customize-prismjs.scss');
 
 exports.onClientEntry = () => {};
